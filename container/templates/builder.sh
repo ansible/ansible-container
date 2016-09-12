320f7dea65462a1fbe3f9724d6ddbdc6f9a4f0a7
Chris Houseknecht <chousekn@redhat.com>
2016-09-11 22:31:48 -0400

    Install any roles included in requirements.yml. - Adds ansible.cfg to `init`. - Sets roles_path to /etc/ansible/roles. - Performs ansible-galaxy install, only if roles added.

--- container/templates/builder.sh
+++ container/templates/builder.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 test '(! -f /src/requirements.txt)' || pip install --no-cache-dir -q -U -r /src/requirements.txt
+
+roles=$(python -c "import yaml; roles = yaml.load(open('./requirement.yml', 'r')); print 0 if not roles else len(roles)")
+if [ ${roles} > 0 ]; then
+    ansible-galaxy install -r ./requirements.yml
+fi
+
 "$@"
