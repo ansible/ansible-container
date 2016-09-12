8e38a1c84b41ff35a67ae50e3b84e20f2bbba13d
Chris Houseknecht <chousekn@redhat.com>
2016-09-11 22:48:09 -0400

    requirements.yml

--- container/templates/builder.sh
+++ container/templates/builder.sh
@@ -2,7 +2,7 @@
 
 test '(! -f /src/requirements.txt)' || pip install --no-cache-dir -q -U -r /src/requirements.txt
 
-roles=$(python -c "import yaml; roles = yaml.load(open('./requirement.yml', 'r')); print 0 if not roles else len(roles)")
+roles=$(python -c "import yaml; roles = yaml.load(open('./requirements.yml', 'r')); print 0 if not roles else len(roles)")
 if [ ${roles} > 0 ]; then
     ansible-galaxy install -r ./requirements.yml
 fi
