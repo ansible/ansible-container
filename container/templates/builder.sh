f8d3292621e1d62c5049d5680580bd9f7e3b031d
Sean Summers <ssummer3@nd.edu>
2016-11-18 01:31:48 -0500

    run pip on ./requirements.txt (#311)
    Run pip on ./requirements.txt.
    
    Match the test format for the rest of the file, and use the same path as the requirements.yml stanza.

--- container/templates/builder.sh
+++ container/templates/builder.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-test '(! -f /src/requirements.txt)' || pip install --no-cache-dir -q -U -r /src/requirements.txt
+if [ -s "./requirements.txt" ]; then
+    pip install --no-cache-dir -q -U -r ./requirements.txt
+fi
 
 if [ -f "./requirements.yml" ]; then
     roles=$(python -c "import yaml; roles = yaml.load(open('./requirements.yml', 'r')); print 0 if not roles else len(roles)")
