431267b0d085781d96f9fdb59328896ecf08bef5
Ryan Brown <sb@ryansb.com>
2016-12-14 03:49:50 -0500

    Python 3-ify the `pre-commit` script so it works on py3-only hosts (#338)

--- update-authors.py
+++ update-authors.py
@@ -14,13 +14,12 @@ user_scores = defaultdict(int)
 
 git_log = subprocess.check_output("git log --shortstat --no-merges --pretty='%aN <%aE>'",
                                   shell=True)
-log_entries = git_log.strip().split('\n')
+log_entries = git_log.decode('utf-8').strip().split('\n')
 while log_entries:
     author = log_entries.pop(0)
     _ = log_entries.pop(0)
     commit_line = log_entries.pop(0)
     commit_parts = [s.strip() for s in commit_line.split(', ')]
-    commit_data = {'files': 0, 'insertions': 0, 'deletions': 0}
     for clause in commit_parts:
         count, action = clause.split(' ', 1)
         if action.endswith('(+)'):
