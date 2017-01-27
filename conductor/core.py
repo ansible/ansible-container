38e8b7803fd5d97543d72a11f39dad08165e9fbb
Joshua "jag" Ginsberg <jag@flowtheory.net>
2017-01-27 10:50:28 +0100

    Totally non-functional checkpoint commit.

--- conductor/core.py
+++ conductor/core.py
@@ -16,9 +16,9 @@ import psutil
 
 import delegator
 
-def load_engine(self, engine_name):
+def load_engine(self, engine_name, project_name, services):
     mod = importlib.import_module('%s.engine' % engine_name, package='.')
-    return mod.Engine()
+    return mod.Engine(project_name, services)
 
 def resolve_role_to_path(role_name):
     # FIXME - How do I programatically resolve the role name into a path?
@@ -83,7 +83,7 @@ def apply_role_to_container(role, container_id, service, engine):
         shutil.rmtree(tmpdir, ignore_errors=True)
 
 def build(engine_name, project_name, services, cache=True):
-    engine = load_engine(engine_name)
+    engine = load_engine(engine_name, project_name, services)
     logger.info('%s integration engine loaded. Build starting.',
                 engine.display_name())
 
@@ -121,7 +121,6 @@ def build(engine_name, project_name, services, cache=True):
 
                 container_id = engine.run_container(
                     image_id=cur_image_id,
-                    project_name=project_name,
                     service_name=name,
                     user='root',
                     working_dir='/',
@@ -138,7 +137,6 @@ def build(engine_name, project_name, services, cache=True):
                 engine.stop_container(container_id)
                 metadata = get_metadata_from_role(role)
                 image_id = engine.commit_role_as_layer(container_id,
-                                                       project_name,
                                                        name,
                                                        fingerprint_hash.hexdigest(),
                                                        metadata)
@@ -149,3 +147,39 @@ def build(engine_name, project_name, services, cache=True):
         else:
             logger.info('%s: No roles specified. Nothing to do.', name)
     logger.info('All images successfully built.')
+
+def run(engine_name, project_name, services):
+    engine = load_engine(engine_name, project_name, services)
+    logger.info('%s integration engine loaded. Preparing run.',
+                engine.display_name())
+
+    # Verify all images are built
+    for service_name in services:
+        logger.info('Verifying image for %s', service_name)
+        image_id = engine.get_latest_image_id_for_service(service_name)
+        if not image_id:
+            logger.error('Missing image for %s. Run "ansible-container build" '
+                         'to (re)create it.', service_name)
+            raise RuntimeError('Run failed.')
+
+    engine.orchestrate()
+
+def restart(engine_name, project_name, services):
+    engine = load_engine(engine_name, project_name, services)
+    logger.info('%s integration engine loaded. Preparing to restart containers.',
+                engine.display_name())
+    engine.restart_all_containers()
+    logger.info('All services restarted.')
+
+def stop(engine_name, project_name, services):
+    engine = load_engine(engine_name, project_name, services)
+    logger.info('%s integration engine loaded. Preparing to stop all containers.',
+                engine.display_name())
+    for service_name in services:
+        container_id = engine.get_container_id_for_service(service_name)
+        if container_id:
+            logger.debug('Stopping %s...', service_name)
+            engine.stop_container(container_id)
+    logger.info('All services stopped.')
+
+
--- conductor/engine.py
+++ conductor/engine.py
@@ -11,6 +11,10 @@ class BaseEngine(object):
     into Ansible Container.
     """
 
+    def __init__(self, project_name, services):
+        self.project_name = project_name
+        self.services = services
+
     @property
     def display_name(self):
         return __name__.split('.')[-2]
@@ -20,19 +24,18 @@ class BaseEngine(object):
         """Additional commandline arguments necessary for ansible-playbook runs."""
         raise NotImplementedError()
 
-
-
     def run_container(self,
                       image_id,
-                      project_name,
                       service_name,
                       **kwargs):
+        """Run a particular container. The kwargs argument contains individual
+        parameter overrides from the service definition."""
         raise NotImplementedError()
 
-    def stop_container(self, *args, **kwargs):
+    def stop_container(self, container_id):
         raise NotImplementedError()
 
-    def restart_container(self, *args, **kwargs):
+    def restart_all_containers(self):
         raise NotImplementedError()
 
     def inspect_container(self, container_id):
@@ -41,16 +44,25 @@ class BaseEngine(object):
     def delete_container(self, container_id):
         raise NotImplementedError()
 
+    def get_container_id_for_service(self, service_name):
+        raise NotImplementedError()
+
     def get_image_id_by_fingerprint(self, fingerprint):
         raise NotImplementedError()
 
     def get_image_id_by_tag(self, tag):
         raise NotImplementedError()
 
+    def get_latest_image_id_for_service(self, service_name):
+        raise NotImplementedError()
+
     def commit_role_as_layer(self,
                              container_id,
-                             project_name,
                              service_name,
                              fingerprint,
                              metadata):
         raise NotImplementedError()
+
+    def orchestrate(self):
+        raise NotImplementedError()
+
