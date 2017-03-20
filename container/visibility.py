280b9d8bc9c5286481cdf49e32954563bf293538
Ryan S. Brown <sb@ryansb.com>
2017-03-20 13:51:24 -0400

    Instead of manually unrolling ordereddict types...
    ...dump them to JSON to retain order.

--- container/visibility.py
+++ container/visibility.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 import inspect
 import logging
 import sys
+import json
 
 from ruamel import ordereddict
 
@@ -28,25 +29,12 @@ def local_var_info(logger, call_name, event_dict):
     })
     return event_dict
 
-def _unroll_odict(od):
-    """Takes an ordereddict and rebuilds it as a regular dict
-
-    Only used in DEBUG mode, because logs below DEBUG are encoded
-    as JSON anyway."""
-    cleaned = {}
-    for key, value in od.items():
-        if isinstance(value, ordereddict.ordereddict):
-            cleaned[key] = _unroll_odict(value)
-        else:
-            cleaned[key] = value
-    return cleaned
-
 def unorder_dict(logger, call_name, event_dict):
     if logger.getEffectiveLevel() > logging.DEBUG:
         return event_dict
     for key, value in event_dict.items():
         if isinstance(value, ordereddict.ordereddict):
-            event_dict[key] = _unroll_odict(value)
+            event_dict[key] = json.dumps(value)
     return event_dict
 
 def add_caller_info(logger, call_name, event_dict):
