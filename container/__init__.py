1f740a5d6cef33178fe59f6338cbc0c3112ab7db
Joshua "jag" Ginsberg <jag@flowtheory.net>
2017-03-23 20:59:38 -0400

    Consolidated cli libraries.

--- container/__init__.py
+++ container/__init__.py
@@ -4,7 +4,26 @@ from __future__ import absolute_import
 __version__ = '0.9.0-pre'
 
 import os
+import functools
 
 # Many features of Ansible Container are flagged on whether or not we're running
 # inside the Conductor container or not. This is the flag.
 ENV = 'conductor' if os.environ.get('ANSIBLE_CONTAINER') else 'host'
+
+def conductor_only(fn):
+    @functools.wraps(fn)
+    def __wrapped__(*args, **kwargs):
+        if ENV != 'conductor':
+            raise EnvironmentError('This method can only be run inside of the '
+                                   'conductor container.')
+        return fn(*args, **kwargs)
+    return __wrapped__
+
+def host_only(fn):
+    @functools.wraps(fn)
+    def __wrapped__(*args, **kwargs):
+        if ENV != 'host':
+            raise EnvironmentError('This method can only be run outside of the '
+                                   'conductor container.')
+        return fn(*args, **kwargs)
+    return __wrapped__
--- container/cli.py
+++ container/cli.py
@@ -7,14 +7,22 @@ logger = getLogger(__name__)
 import os
 import sys
 import argparse
+import base64
+import json
 
 import requests.exceptions
 
-from . import core
-from . import config
-from . import exceptions
-from .utils import load_shipit_engine, AVAILABLE_SHIPIT_ENGINES
+import container
 from .conductor import exceptions as conductor_exc
+
+if container.ENV == 'host':
+    from . import core
+    from . import config
+    from . import exceptions
+    from .utils import load_shipit_engine, AVAILABLE_SHIPIT_ENGINES
+elif container.ENV == 'conductor':
+    from .conductor.config import AnsibleContainerConductorConfig
+    from .conductor import core as conductor_core
 
 from logging import config
 LOGGING = {
@@ -205,6 +213,7 @@ class HostCommand(object):
                                help=u'Path to project/context to import.')
 
 
+    @container.host_only
     def __call__(self):
         parser = argparse.ArgumentParser(description=u'Build, orchestrate, run, and '
                                                      u'ship Docker containers with '
@@ -280,3 +289,47 @@ class HostCommand(object):
             sys.exit(1)
 
 host_commandline = HostCommand()
+
+def decode_b64json(encoded_params):
+    return json.loads(base64.decodestring(encoded_params))
+
+@container.conductor_only
+def conductor_commandline():
+    sys.stderr.write('Parsing conductor CLI args.\n')
+    parser = argparse.ArgumentParser(description=u'This should not be invoked '
+                                                 u'except in a container by '
+                                                 u'Ansible Container.')
+    parser.add_argument('command', action='store', help=u'Command to run.',
+                        choices=['build', 'deploy', 'install', 'push', 'run', 'restart',
+                                 'stop'])
+    parser.add_argument('--project-name', action='store', help=u'Project name.', required=True)
+    parser.add_argument('--engine', action='store', help=u'Engine name.', required=True)
+    parser.add_argument('--params', action='store', required=False,
+                        help=u'Encoded parameters for command.')
+    parser.add_argument('--config', action='store', required=True,
+                        help=u'Encoded Ansible Container config.')
+    parser.add_argument('--encoding', action='store', choices=['b64json'],
+                        help=u'Encoding used for parameters.', default='b64json')
+
+    args = parser.parse_args()
+
+    decoding_fn = globals()['decode_%s' % args.encoding]
+    if args.params:
+        params = decoding_fn(args.params)
+    else:
+        params = {}
+
+    if params.get('debug'):
+        LOGGING['loggers']['container']['level'] = 'DEBUG'
+    config.dictConfig(LOGGING)
+
+    containers_config = decoding_fn(args.config)
+    conductor_config = AnsibleContainerConductorConfig(containers_config)
+
+    logger.debug('Starting Ansible Container Conductor: %s', args.command,
+        services=conductor_config.services)
+    getattr(conductor_core, args.command)(args.engine, args.project_name,
+                                          conductor_config.services,
+                                          volume_data=conductor_config.volumes,
+                                          repository_data=conductor_config.registries,
+                                          **params)
